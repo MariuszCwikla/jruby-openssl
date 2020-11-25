28d73395b53c7d1ab6ccaae2f7e440edd825375f
Mariusz Ćwikla <mariuszcwikla@gmail.com>
2020-11-25 14:58:04 +0100

    fix test

--- src/test/ruby/ssl/test_context.rb
+++ src/test/ruby/ssl/test_context.rb
@@ -189,7 +189,7 @@ class TestSSLContext < TestCase
     context.ciphers = "AES"
 
     actual = context.ciphers.map { |cipher| cipher[0]}
-    assert actual.include?("ECDHE-ECDSA-AES128-GCM-SHA256")
+    assert actual.include?("ECDHE-RSA-AES128-SHA")
     assert actual.include?("ECDHE-ECDSA-AES128-SHA")
     assert actual.include?("AES128-SHA")
   end
